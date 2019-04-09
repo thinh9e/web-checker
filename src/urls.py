b5a2c948e50aeb1cde797c97aad6e69de857b906
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2019-04-10 00:55:58 +0700

    import handle, path index

--- src/urls.py
+++ src/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import handler400, handler403, handler404, handler500
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('checkweb.urls')),
     path('admin/', admin.site.urls),
     path('robots.txt', views.Robots.as_view(), name='robots'),
 ]
