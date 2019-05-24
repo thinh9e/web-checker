6e98334a625e8ebba96f9ea0d5831d8ab84e6485
Nguyen Phuoc Thinh <npthinh1996@gmail.com>
2019-05-24 17:15:48 +0700

    add page check demo & redirect if error

--- checkweb/views.py
+++ checkweb/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.views.generic import TemplateView
 from .functions import parsing, reCaptcha
 
@@ -16,8 +17,26 @@ class ContactView(TemplateView):
 
 
 class CheckView(TemplateView):
-    template_name = 'checkweb/index.html'
-    template_success = 'checkweb/check.html'
+    template_name = 'checkweb/check.html'
+    template_error = 'checkweb/index.html'
+
+    def get(self, request):
+        context = {
+            'title': 'Phân tích & Đánh giá SEO cho website của bạn | Đánh Giá Web',
+            'description': 'Trang phân tích, đánh giá SEO cho website nhanh chóng, chính xác và miễn phí',
+            'pageRank': 0,
+            'favicon': 'https://danhgiaweb.top/static/img/favicon.png',
+            'robotsMeta': 'index, follow, noodp, noydir',
+            'h1Tags': ['Phân tích & Đánh giá SEO'],
+            'h2Tags': [],
+            'robotsTxt': 'https://danhgiaweb.top/robots.txt',
+            'sitemaps': ['https://danhgiaweb.top/sitemap.xml'],
+            'aBrokens': [],
+            'cssInlines': [],
+            'missAlts': [],
+            'url': 'https://danhgiaweb.top/',
+        }
+        return render(request, self.template_name, context)
 
     def post(self, request):
         url = request.POST['url']
@@ -27,7 +46,12 @@ class CheckView(TemplateView):
             context = parsing(url)
             if context:
                 context['url'] = url
-                return render(request, self.template_success, context)
-            return render(request, self.template_name, {'errURL': True, 'url': url})
-
-        return render(request, self.template_name, {'errCC': True, 'url': url})
+                return render(request, self.template_name, context)
+            messages.info(request, url)
+            messages.error(
+                request, '* Không phân tích được URL. Vui lòng kiểm tra lại!')
+            return redirect('/')
+        messages.info(request, url)
+        messages.error(
+            request, '* Bạn chưa được kiểm tra không phải là robot!')
+        return redirect('/')
