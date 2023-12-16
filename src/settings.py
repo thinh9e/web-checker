64715919fff9e226117913f3a72146ecf9e2671c
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2023-12-16 17:55:41 +0700

    add ASGI server

--- src/settings.py
+++ src/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'src/assets']
+STATIC_ROOT = BASE_DIR / 'static_root'
+STATICFILES_DIRS = [BASE_DIR / 'src/static']
 
 
 # Default primary key field type
--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.conf.urls import handler400, handler403, handler404, handler500
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 from . import views
 
 urlpatterns = [
@@ -10,6 +13,9 @@ urlpatterns = [
     path('sitemap.xml', views.Sitemap.as_view(), name='sitemap'),
     path('', include('checker.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 
 handler400 = views.bad_request
 handler403 = views.permission_denied
