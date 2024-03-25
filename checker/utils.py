a60f0aff75ec628caa51a2868a66981d97736bd3
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2024-03-25 23:18:41 +0700

    add get robots link

--- checker/utils.py
+++ checker/utils.py
@@ -1,8 +1,10 @@
 from json import JSONDecodeError
+from typing import Optional
 
 import requests
 from django.conf import settings
-from requests.exceptions import RequestException
+from requests import Session
+from requests.exceptions import HTTPError
 
 REQUEST_TIMEOUT = 30
 
@@ -30,6 +32,24 @@ def verify_captcha(response: str, user_ip: str) -> bool:
         verify = requests.post(url=url, data=data)
         result: dict = verify.json()
         return result.get("success")
-    except RequestException | JSONDecodeError as e:
+    except HTTPError | JSONDecodeError as e:
         print(f"Failed to verify reCAPTCHA: {e}")
         return False
+
+
+def get_robots_link(client: Session, base_url: str) -> Optional[str]:
+    """
+    Retrieves the URL of the robots.txt file for a given base URL using the provided HTTP client.
+
+    :param client: The HTTP client session to use for making the request.
+    :param base_url: The base URL for which to retrieve the robots.txt file.
+    :return: The URL of the robots.txt file if it exists and can be accessed, None otherwise.
+    """
+    robots_url = base_url + "/robots.txt"
+    try:
+        r = client.head(robots_url)
+        r.raise_for_status()
+        return robots_url
+    except HTTPError as e:
+        print(f"Failed to get robots.txt: {e}")
+        return None
