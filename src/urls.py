ff0f106d0b00dfd93eac8472e04b950a1e508d1c
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2025-02-09 21:08:26 +0700

    refactor: reformat code

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,7 @@
 from django.conf import settings
-from django.conf.urls import handler400, handler403, handler404, handler500
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 from . import views
 
